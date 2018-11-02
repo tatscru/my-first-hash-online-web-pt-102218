5f99f57fea18ac1c4d06d3601da640ed78de8b8c
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 15:19:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash {
-  your_hash = 
-"names" => ""
+  pod_mates = "names" => "michael", "zahra", "tatiana"
 }
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
