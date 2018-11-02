3f441f06fe607f8ff6123a3d3614441d7fe759d0
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 15:51:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,7 @@ end
 def shipping_manifest
   the_manifest = {
     "porcelain vases" => 2,
-    "whale bone coresets" => 5, 
+    "whale bone corsets" => 5, 
     "oil paintings" => 3
   }
  the_manifest
