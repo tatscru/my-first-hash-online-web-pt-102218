a5f7475ac89a68dfc046b1fe4a1192f94f953cf1
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 15:39:45 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,6 +13,7 @@ def shipping_manifest
     "porcelain vases" => 2, 
   }
     puts shipping_manifest
+    
   # puts shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
