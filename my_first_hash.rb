d451cf49c8cf5f6b3d25a88ed16e196e15ae3b96
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 16:04:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,7 +40,8 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest[""]
+shipping_manifest["muskets"]= 2 
+
 
   # add 4 gun powder to the shipping_manifest hash below
 
