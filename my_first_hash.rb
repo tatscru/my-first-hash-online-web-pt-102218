857e062678f7188a287b028cf185f817a85975cd
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 15:33:26 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,12 +12,11 @@ def shipping_manifest
     "porcelain vases" => 2, 
     "oil paintings" => 3
   }
+end 
   puts shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
 end
 
 def retrieval
