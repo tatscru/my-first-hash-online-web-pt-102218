93f3b390103bf1f377ca93685561a46176705b2f
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 15:18:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,7 @@
-def my_hash
+def my_hash {
+  your_hash = 
+  "names" => ""
+}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
